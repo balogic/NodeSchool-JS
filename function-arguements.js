3f9b9de52e1c303bbf3681befd1eeb552a377519
Balaji Ravichandran <rbalajives@gmail.com>
2015-08-17 23:37:25 +0530

    initial commit

---
+++ function-arguements.js
@@ -0,0 +1,7 @@
+function math(add, sub, mul) {
+	var sec = sub * mul;
+	var out = sec + add;
+	console.log(out);
+}
+
+math(53,61,67);
---
+++ number-to-string.js
@@ -0,0 +1,3 @@
+var n = 128;
+v = n.toString();
+console.log(v);
